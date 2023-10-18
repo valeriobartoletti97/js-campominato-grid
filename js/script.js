07e46d9a435d9ec2c17fbd3e009dae64ad98bb93
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2023-10-18 13:58:56 +0200

    start with a template

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const btn = document.querySelector('button');
+
+
+btn.addEventListener('click', function(){
+    /* numero di quadratini da generare */
+    const numSquare = 100;
+    /* mi prendo la griglia di gioco */
+    const playground = document.getElementById('playground');
+    playground.innerHTML= '';
+    /* ciclo per stampare i quadratini */
+    for(let i = 0; i < numSquare; i++){
+        /* genero quadratino */
+        let square = drawSquare(i,numSquare);
+
+        /* appendo il quadratino alla griglia */
+        playground.append(square);
+    }
+});
+
+/* disegno quadrato */
+function drawSquare(squareIndex,numSquare){
+    let squareWidth = Math.sqrt(numSquare);
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.width = `calc(100% / ${squareWidth})`;
+    square.style.height = square.style.width;
+    square.innerHTML = squareIndex + 1;
+    square.addEventListener('click',function(){
+        square.classList.add('active');
+        console.log(squareIndex + 1) 
+    });
+    return square;
+
+}
+
